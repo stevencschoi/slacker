f13cb38d05e9fb1d931c6ecce2058dbae52208dc
Steven Choi <stevencschoi@gmail.com>
2020-05-05 20:59:53 -0400

    boiler plate added from create react app

---
+++ src/components/partials/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img src="" alt="Insert Logo Here"></img>
+      </div>
+    </header>
+  );
+}
