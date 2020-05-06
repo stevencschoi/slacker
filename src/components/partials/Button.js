43bb94e04a5bb67c263edc36adec753db4bba091
Steven Choi <stevencschoi@gmail.com>
2020-05-05 23:32:22 -0400

    adding form inputs as components

--- src/components/partials/Button.js
+++ src/components/partials/Button.js
@@ -4,7 +4,7 @@ const classNames = require("classnames");
 
 export default function Button(props) {
   // add class to button based on props
-  const buttonClass = classNames("button", {
+  const buttonClass = classNames({
     "btn btn-sm follow btn-primary": props.default,
     "login-button": props.loginButton,
   });
--- src/components/partials/Login.js
+++ src/components/partials/Login.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/partials/Login.scss";
+import Input from "../partials/Input";
 import Button from "../partials/Button";
 
 export default function Login() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Button clicked!");
   };
+
+  const handleInputChange = (e) => {
+    e.persist();
+    setInputs((inputs) => ({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
-    <form className="user-info" onSubmit={handleSubmit}>
+    <form userInfo onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={inputs.username}
+        onChange={handleInputChange}
+      />
+      <Input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={inputs.password}
+        onChange={handleInputChange}
+      />
       <Button loginButton type="submit">
         Login
       </Button>
